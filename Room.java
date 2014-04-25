010ad95388bbbef95f4d1ad9b7f6d351fce25979
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-25 01:35:04 -0400

    Got TreeSet to compile, need to add interval class

---
+++ Room.java
@@ -0,0 +1,16 @@
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Room {
+	public static void main(String[] args) {
+		System.out.print("I am a room!\n");
+	
+		SortedSet set = new TreeSet();
+		/* set.add(new Date());
+		Iterator it = set.iterator();
+		while (it.hasNext()) {
+			Object interval = it.next();
+			System.out.println(element.toString());
+		} */
+	}
+}
