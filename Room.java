3de228c4c5684546172b6b6b869cf111bfdaac09
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-26 13:25:27 -0400

    Tried to use iterators

--- Room.java
+++ Room.java
@@ -1,11 +1,15 @@
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.*;
 
 public class Room {
 	public static void main(String[] args) {
 		System.out.print("I am a room!\n");
 	
 		SortedSet set = new TreeSet();
+	//		set.add("sup");
+
+		Iterator it = set.iterator();
 		/* set.add(new Date());
 		Iterator it = set.iterator();
 		while (it.hasNext()) {
