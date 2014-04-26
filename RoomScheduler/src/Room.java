acb3a7baf7364f8b7ac918c65bc4cc497ee967a8
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-26 18:51:42 -0400

    Switched to Eclipse to be compatible with Joda

---
+++ RoomScheduler/src/Room.java
@@ -0,0 +1,12 @@
+import org.joda.time.*;
+
+public class Room {
+
+	public static void main(String[] args) {
+		System.out.print("I'm a room!\n");
+	
+		DateTime now = new DateTime();
+		System.out.println(now);
+	}
+
+}
