fa16218183490950eb98303dec338fc4cea75cbf
Vishaal Kalwani <vishaalk@umich.edu>
2014-04-28 01:02:45 -0400

    Did some basic while loop stuff

--- RoomScheduler/src/Room.java
+++ RoomScheduler/src/Room.java
@@ -1,7 +1,9 @@
 import org.joda.time.*;
 
 public class Room {
-
+	//variables
+	private String name;
+	
 	public static void main(String[] args) {
 		System.out.print("I'm a room!\n");
 	
@@ -9,5 +11,11 @@ public class Room {
 		//System.out.println(now);
 		System.out.println(now.toString("hh:mm:SS MM/dd/YYYY"));
 	}
+	
+	//public interface
+	public String get_name() {
+		return name;
+	}
+	
 
 }
---
+++ RoomScheduler/src/RoomScheduler.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class RoomScheduler {
+	private static TreeMap<String, Integer> Rooms;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Meeting Scheduler!");
+		Scanner cin = new Scanner(System.in);
+		while (true) {
+			System.out.print("Enter command: ");
+			String line = cin.next();
+			//Rooms.put(line, 5);
+			System.out.println(line);
+		}
+	}
+
+}
