e6d3cd1c77dae0ff588ffdd826e7f2270e2f0711
Vishaal Kalwani <vishaalk@umich.edu>
2014-05-10 19:47:43 -0400

    git is weird

--- RoomScheduler/src/LocalDateTimeInterval.java
+++ RoomScheduler/src/LocalDateTimeInterval.java
@@ -9,9 +9,20 @@ public class LocalDateTimeInterval {
 		start_time = start;
 		end_time = end;
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I'm an interval!");
+		LocalDateTime start = new LocalDateTime();
+		LocalDateTime end = new LocalDateTime();
+		LocalDateTimeInterval test = new LocalDateTimeInterval(start, end);
+		System.out.println("Reservation: " + test);
 	}
+	
 
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		result.append(start_time.toString() + " - " + end_time.toString());
+		return result.toString();
+	}
 }
--- RoomScheduler/src/RoomReservation.java
+++ RoomScheduler/src/RoomReservation.java
@@ -2,6 +2,12 @@ public class RoomReservation {
 	private LocalDateTimeInterval period;
 	private String name;
 	
+	//only one constructor possible
+	RoomReservation(String name_, LocalDateTimeInterval period_) {
+		name = name_;
+		period = period_;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("I'm a reservation!");
 		
